bb092215d9df12f42fea547a50618d8b1aa7cda8
Kamogelo Mahlake <3949843@myuwc.ac.za>
2025-01-29 10:07:33 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
 
-var app = express();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +23,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size:req.file.size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
